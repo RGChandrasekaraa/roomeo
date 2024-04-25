e9f99bd970c545285bf5c3ca6cb07f8e339fa6c4
Chandrasekaraa Ramadoss Ganesh <94929823+RGChandrasekaraa@users.noreply.github.com>
2024-04-25 23:32:38 +1000

    single page skeleton done.

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,8 +1,40 @@
+import Slider from '../../components/slider/slider'
 import './singlePage.scss'
+import {singlePostData, userData} from "../../lib/dummyData"
 
 function SinglePage(){
   return (
-    <div className='singlePage'>SinglePage</div>
+    <div className='singlePage'>
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  {/* insert address img from online */}
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">
+                  {singlePostData.price}
+                </div>
+              </div>
+              <div className="user">
+                  <img src="https://i.ytimg.com/vi/wAnz3f9LfqQ/maxresdefault.jpg" alt='user' />
+                  <span>{userData.name}</span>
+              </div>
+          </div>
+          </div>
+          <div className="bottom">
+            <p>{singlePostData.description}</p>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
   )
 }
 
